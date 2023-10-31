dee2969be1e660142632035992d75c47c92f5370
minwoo <minwoo9621@gmail.com>
2023-10-31 20:51:49 +0900

    Add posts apps in settings.py and model in admin site

--- list_page_project/posts/admin.py
+++ list_page_project/posts/admin.py
@@ -1,3 +1,7 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Post
+
+admin.site.register(Post)
+admin.site.register(Comment)
