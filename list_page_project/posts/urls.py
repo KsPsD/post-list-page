cc8a43666d7b2dc80c558e432691ce03312585eb
minwoo <minwoo9621@gmail.com>
2023-11-01 17:04:32 +0900

    Add PostDetail view and template

--- list_page_project/posts/urls.py
+++ list_page_project/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from posts.views.front import PostCreateView, post_list
+from posts.views.front import PostCreateView, PostDetailView, post_list
 
 urlpatterns = [
     path("", post_list, name="post_list"),
     path("create/", PostCreateView.as_view(), name="post_create"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
 ]
